616af50b39987b4b07e4c6482652e57e54b38aa6
bschappaugh <brandonschappaugh98@gmail.com>
2022-12-19 15:25:08 -0500

    added list accounts function

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,10 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_list_all_accounts(self):
+        """It should list all accounts"""
+        self._create_accounts(10)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 10)
